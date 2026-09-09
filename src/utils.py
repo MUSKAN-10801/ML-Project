9687ed7e544fcd601c63dae1fcca8cb508f2ff9d
MUSKAN-10801 <muskangupta08022007@gmail.com>
2026-09-10 00:25:39 +0530

    Model Trainer

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import numpy as np
 
 from src.exception import CustomException
 import dill
+from sklearn.metrics import r2_score
 
 
 
@@ -16,6 +17,30 @@ def save_object(file_path:str,obj):
 
         with open(file_path,'wb') as file_obj:
             dill.dump(obj,file_obj)
+
+    except Exception as e:
+        raise CustomException(e,sys)
+
+def evaluate_models(X_train,y_train,X_test,y_test,models):
+    try:
+        report = {}
+        for i in range(len(models)):
+            model = list(models.values())[i]
+            # Train model
+            model.fit(X_train,y_train)
+
+            y_train_pred = model.predict(X_train)
+            # Predict Testing data
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train,y_train_pred)
+
+            # Get R2 scores for train and test data
+            test_model_score = r2_score(y_test,y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
 
     except Exception as e:
         raise CustomException(e,sys)
